736ecc0a64422d3599884b900407d1bbfd5d21b4
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-07-19 22:40:56 +0100

    added styling to searchbox

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const SearchBox = () => {
-  return <input type="search" placeholder="Search robots" />;
+  return (
+    <div>
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="Search robots"
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
