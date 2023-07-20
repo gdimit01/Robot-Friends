029f3460d9c56a7c47d6f38f1e920c2e830734f5
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-07-20 09:36:43 +0100

    updated the app and searchbox.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 
+/* This code defines a class called `App` that extends the `Component` class from the React library.
+The `constructor` method is used to initialize the state of the `App` component. */
 class App extends Component {
   constructor() {
     super();
@@ -12,11 +14,25 @@ class App extends Component {
     };
   }
 
+  /**
+   * Handles the change event when the search input value is modified.
+   *
+   * @param {Event} event - The change event.
+   */
+  onSearchChange(event) {
+    console.log(event.target.value);
+  }
+
+  /**
+   * Renders the Robot Friends component.
+   *
+   * @return {JSX.Element} The rendered Robot Friends component.
+   */
   render() {
     return (
       <div className="tc">
         <h1>Robot Friends</h1>
-        <SearchBox />
+        <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={this.state.robots} />
       </div>
     );
